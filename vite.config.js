3a33c0dff2017fee9dc2f77068daf50f3f8834b7
Yu-ZhL <qqtc3412@gmail.com>
2026-01-22 11:53:49 +0800

    首次提交：从宝塔服务器上传完整代码

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+
+    server: {
+        host: '0.0.0.0', // 允许外部访问
+        hmr: {
+            host: '103.214.173.247', // 热更新连接的IP
+        },
+        watch: {
+            usePolling: true, // 监控文件变化
+        }
+    },
+});
