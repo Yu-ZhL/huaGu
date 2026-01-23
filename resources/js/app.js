96b37d82ffa573e78f6090fee9327430cc4880a4
soketIoc <1207162437@qq.com>
2026-01-23 16:02:14 +0800

    测试

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
 
+console.log(123123)
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>飞猫选品</title>
-    @routess
     @vite(['resources/js/app.js'])
 </head>
 <body>
