9cfcc4d41c3ae5942717538633379aa3b7675456
soketIoc <1207162437@qq.com>
2026-01-23 18:19:28 +0800

    测试

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
 import { createApp } from 'vue'
-import ProductList from './components/ProductList.vue'
+import ProductList from '@/components/ProductList.vue'
 
 createApp(ProductList).mount('#app')
+
