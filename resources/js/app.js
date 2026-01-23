eae3a7e9a5aadf677062e7541142eb4422181829
soketIoc <1207162437@qq.com>
2026-01-23 16:27:11 +0800

    测试

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,8 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { createPinia } from 'pinia';
-
+import App from './App.vue'
+import router from './router'
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -27,3 +28,14 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+
+
+
+
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
