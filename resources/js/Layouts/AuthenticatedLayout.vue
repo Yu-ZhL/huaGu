77d9bad8c9aa19143c98bcf6a8b6f54971d31455
soketIoc <1207162437@qq.com>
2026-01-23 12:02:44 +0800

    wdq

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -9,7 +9,7 @@ import { Link } from '@inertiajs/vue3';
 
 const showingNavigationDropdown = ref(false);
 </script>
-
+ 
 <template>
     <div>
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
