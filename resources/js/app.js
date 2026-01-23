c8dd84602f216892d90fd9a10759cb5347a22cd5
soketIoc <1207162437@qq.com>
2026-01-23 17:14:14 +0800

    测试

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,6 @@
 import '../css/app.css';
 import './bootstrap';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import { createPinia } from 'pinia';
-import App from './App.vue'
-import router from './router'
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { createApp } from 'vue'
+import ProductList from './components/ProductList.vue'
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        const pinia = createPinia();
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .use(pinia)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
-
-
-
-
-const app = createApp(App)
-const pinia = createPinia()
-
-app.use(pinia)
-app.use(router)
-
-app.mount('#app')
+createApp(ProductList).mount('#app')
