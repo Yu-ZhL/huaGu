c7645950aaf6f00f833bee72b59322854a7403a4
soketIoc <1207162437@qq.com>
2026-01-23 16:33:43 +0800

    测试

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,21 @@ export default defineConfig({
             },
         }),
     ],
-
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+    build: {
+        outDir: 'public/build',
+        assetsDir: 'assets',
+        manifest: true,
+        rollupOptions: {
+            input: {
+                app: './resources/js/app.js'
+            }
+        }
+    },
     server: {
         host: '0.0.0.0', // 允许外部访问
         hmr: {
