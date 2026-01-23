2561b145b589c74674669fb60a849f65bbb218d4
soketIoc <1207162437@qq.com>
2026-01-23 17:07:10 +0800

    测试

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './resources/js'),
+            '@': path.resolve(__dirname, '/resources/js'),
         },
     },
     server: {
