3dda5d26d58df893fd89fba8edb0be312bf55b0f
soketIoc <1207162437@qq.com>
2026-01-23 16:27:40 +0800

    测试

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,8 +11,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @routess
-        @vite(['resources/js/app.js'])
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
