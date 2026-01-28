f0bce4770745f6f708ad27c509a5d9fc92ec2ede
YuZhl <3412498608@qq.com>
2026-01-28 15:20:42 +0800

    Fix HeaderBar syntax and add heroicons dependency

--- resources/js/Components/HeaderBar.vue
+++ resources/js/Components/HeaderBar.vue
@@ -9,7 +9,7 @@ defineEmits(['toggleSidebar'])
     <header class="bg-white shadow-sm border-b sticky top-0 z-20">
         <div class="flex items-center justify-between px-6 py-3">
             <div class="flex items-center">
-                <button @click=" $ emit('toggleSidebar')" class="mr-4 text-gray-500 hover:text-gray-700">
+                <button @click="$emit('toggleSidebar')" class="mr-4 text-gray-500 hover:text-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
